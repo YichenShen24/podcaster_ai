aa1ee84c62652e183ead3a84acb4abe5ae7f26d6
Yichen(Snow) Shen <snowsyc524@gmail.com>
2025-03-11 12:08:30 -0400

    first commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://sweet-magpie-80.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
